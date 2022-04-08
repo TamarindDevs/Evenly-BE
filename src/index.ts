bb9b78650e5f31a90111c38b31b3c2cbf6a57990
Williams <billmal071@gmail.com>
2022-04-09 00:49:01 +0100

    no-plusplus: ["error", { "allowForLoopAfterthoughts": true }]

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import express from "express";
+import { cpus } from "os";
+import cluster from "cluster";
+import cors from "cors";
+
+const numCPUs = cpus().length;
+
+if (cluster.isPrimary) {
+  console.info(`Primary ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker) => {
+    console.info(`worker ${worker.process.pid} died`);
+    cluster.fork();
+  });
+} else {
+  console.info(`Worker ${process.pid} started`);
+
+  const app = express();
+  app.use(cors());
+  app.use(express.json());
+
+  app.get("/", (req, res) => {
+    res.send("Hello world");
+  });
+
+  app.listen(3000, () => {
+    console.info(`Worker ${process.pid} started`);
+  });
+}
