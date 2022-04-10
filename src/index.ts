a5bb1454f51531815cd3e98313420afb21343609
Williams <billmal071@gmail.com>
2022-04-10 23:18:21 +0100

    feat: added mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,46 @@
+/* eslint-disable import/no-named-as-default-member */
+/* eslint-disable import/order */
+/* eslint-disable import/first */
+require("tsconfig-paths/register");
+
 import express, { json } from "express";
-import { cpus } from "os";
-import cluster from "cluster";
 import cors from "cors";
 import { serve, setup } from "swagger-ui-express";
 import yaml from "js-yaml";
-import fs from "fs";
 import helmet from "helmet";
+import { cpus } from "os";
+import cluster from "cluster";
+import fs from "fs";
+import path from "path";
 import "express-async-errors";
 import "dotenv/config";
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 // user defined
 import config from "@config/config";
 import globalExceptionLayer from "@error/errorHandler";
-import path from "path";
+import connect from "@db/db";
+import logger from "@helpers/logger";
+import morganMiddleware from "@middlewares/morgan.middleware";
 
-require("tsconfig-paths/register");
+const app = express();
+// connect db
+connect();
+// sentry init
+Sentry.init({
+  dsn: config.sentry_dsn,
+  attachStacktrace: true,
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Tracing.Integrations.Express({
+      // to trace all requests to the default router
+      app,
+    }),
+  ],
+  tracesSampleRate: 1.0,
+});
 
 // Get document, or throw exception on error
 let swaggerDocument: any;
@@ -22,13 +49,13 @@ try {
     fs.readFileSync(path.resolve(__dirname, "docs/swagger.yml"), "utf8"),
   );
 } catch (e) {
-  console.error(e);
+  logger.error(e);
 }
 
 const numCPUs = cpus().length;
 
 if (cluster.isPrimary) {
-  console.info(`Primary ${process.pid} is running`);
+  logger.info(`Primary ${process.pid} is running`);
 
   // Fork workers.
   for (let i = 0; i < numCPUs; i++) {
@@ -36,20 +63,26 @@ if (cluster.isPrimary) {
   }
 
   cluster.on("exit", (worker) => {
-    console.info(`worker ${worker.process.pid} died`);
+    logger.info(`worker ${worker.process.pid} died`);
     cluster.fork();
   });
 } else {
-  console.info(`Worker ${process.pid} started`);
+  logger.info(`Worker ${process.pid} started`);
 
-  const app = express();
+  // The sentry request handler must be the first middleware on the app
+  app.use(Sentry.Handlers.requestHandler() as express.RequestHandler);
+  // TracingHandler creates a trace for every incoming request
+  app.use(Sentry.Handlers.tracingHandler());
   app.use(helmet());
   app.use(cors());
   app.use(json());
+  app.use(morganMiddleware);
   app.use("/api/v1/docs", serve, setup(swaggerDocument));
 
+  // The sentry error handler must be before any other error middleware and after all controllers
+  app.use(Sentry.Handlers.errorHandler() as express.ErrorRequestHandler);
   app.use(globalExceptionLayer);
   app.listen(config.port, () => {
-    console.info(`Worker ${process.pid} started on port ${config.port}`);
+    logger.info(`Worker ${process.pid} started on port ${config.port}`);
   });
 }
